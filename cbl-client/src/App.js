3b9996c013e9458b09cbb0f48c19ce74ea295713
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-10-26 12:32:42 +0900

    Update App.js

--- cbl-client/src/App.js
+++ cbl-client/src/App.js
@@ -1,3 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+// Layouts
+import MainLayout from "./layout/MainLayout";
+import OrderLayout from "./layout/OrderLayout";
+// Pages
+import MainPage from "./pages/MainPage";
+import About from "./pages/About";
+import BookDetail from "./pages/BookDetail";
+import OrderDetail from "./pages/OrderDetail";
 
 
 function App() {

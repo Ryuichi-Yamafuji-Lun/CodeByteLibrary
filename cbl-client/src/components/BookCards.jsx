eccb7e526ff72b2024f7d448d0eb05e0f9e78207
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-11-01 00:10:44 +0900

    Update BookCards.jsx

--- cbl-client/src/components/BookCards.jsx
+++ cbl-client/src/components/BookCards.jsx
@@ -1,12 +1,18 @@
-const BookCards = ({ title, authors, thumbnail }) => {
+const BookCards = ({ title, authors, thumbnail, isbn, price }) => {
   
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg m-4 bg-white">
-      <img src={thumbnail} alt={title} className="w-full h-48 object-cover" />
-      <div className="px-6 py-4 text-black">
-        <div className="font-bold text-xl mb-2">{title}</div>
-        <p className="text-gray-700 text-base">
-          Authors: {authors.join(", ") || "Unknown"}
+      <img src={thumbnail} alt={title} className="w-full h-48 object-contain bg-secondary-color" />
+      <div className="px-6 py-2 text-black">
+        <div className="font-bold text-xl">{title}</div>
+        <p className="text-gray-700 text-base mb-2">
+          Authors: {authors || "Unknown"}
+        </p>
+        <p className="text-sm">
+          ISBN: {isbn}
+        </p>
+        <p className="text-black">
+          Price: ¥{price}
         </p>
       </div>
     </div>
